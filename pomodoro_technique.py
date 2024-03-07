f87f27e417b45ac9e35374cd1006a7da5e3c8784
Ethan <QustoTemmo@protonmail.com>
2024-03-07 15:38:33 +0100

    Better Code

--- pomodoro_technique.py
+++ pomodoro_technique.py
@@ -1,15 +1,8 @@
 from tkinter import *
 
-PINK = "#e2979c"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
-counter = 0
-reps = 0
-time = "00:00"
-work_secs = 25 * 60
-short_break_secs = 5 * 60 
-long_break_secs = 20 * 60
+PINK = "#e2979c"; RED = "#e7305b"; GREEN = "#9bdeac"; YELLOW = "#f7f5dd"
+counter = 0; reps = 0; time = "00:00"
+work_secs = 25 * 60; short_break_secs = 5 * 60 ; long_break_secs = 20 * 60
 
 def reset():
     global reps, check_marks, time
@@ -21,7 +14,6 @@ def reset():
 
 def start():
     global reps, short_break_secs, long_break_secs, work_secs, counter, time
-    
     reps += 1
     if reps % 8 == 0:
         counter = long_break_secs
@@ -57,25 +49,21 @@ def update_timer_display():
 
 root = Tk()
 root.config(background=YELLOW)
-root.title("Pomodoro Technique")
+root.title("Pomodoro Technique App")
+
+tomate = PhotoImage(file="./tomato.png")
+root.iconphoto(False, tomate)
 
 window_width = 500
 window_height = 500
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-x = (screen_width - window_width) // 2
-y = (screen_height - window_height) // 2
-root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+root.geometry(f"{window_width}x{window_height}+{(root.winfo_screenwidth() - window_width) // 2}+{(root.winfo_screenheight() - window_height) // 2}")
 
 upper_text = Label(text="Timer", font=("Arial", 40, "bold"), foreground=GREEN, background=YELLOW)
 upper_text.grid(column=1, row=0, pady=(25, 0))
 
-tomato = PhotoImage(file="./tomato.png")
-
 timer = Canvas(root, highlightthickness=0, background=YELLOW, width=200, height=220)
-timer.create_image(100, 100, image=tomato)
+timer.create_image(100, 100, image=tomate)
 timer_text = timer.create_text(100, 120, text=time, font=("Arial", 20, "bold"), fill=GREEN)
-
 timer.grid(column=1, row=1, pady=(20, 0))
 
 reset_button = Button(text="Reset", command=reset, bg=GREEN, relief=FLAT, highlightbackground=RED, borderwidth=2)
